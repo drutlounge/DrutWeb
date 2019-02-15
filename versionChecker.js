ed3a9aab3725026d533b471f75121e6fd914ebcd
Brendan <jbrendan70@outlook.com>
2019-02-15 22:34:49 +0000

    Version checker

---
+++ versionChecker.js
@@ -0,0 +1,17 @@
+const https = require('https');
+const url = require('url');
+const fs = require('fs');
+const { log } = require("./index");
+https.get({ host: 'api.github.com', path: '/repos/drutlounge/DrutWeb/commits/master', headers: { 'User-Agent': 'request' } }, function (res) {
+    let json = '';
+    res.on('data', function (chunk) { json += chunk; });
+    res.on('end', function () {
+        var data = JSON.parse(json);
+        const parseName = url.parse(data.html_url);
+        const getPath = parseName.pathname;
+        const formatPath = getPath.replace(/((?!\w+$).)/g, '');
+        if (formatPath != fs.readFileSync('.git/refs/heads/master').toString('utf-8')) {
+            log.error('AN UPDATE IS AVAILABLE. YOU SHOULD PULL CHANGES ASAP!');
+        }
+    });
+});
